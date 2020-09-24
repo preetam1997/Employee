5fe1320cb98f3ddeef026c392abf5d485c4b2fd9
unknown <ompreetam317@gmail.com>
2020-09-25 00:18:57 +0530

    [Preetam]Added:UC1-EmployeePresentAbsent

--- Employee.java
+++ Employee.java
@@ -2,7 +2,14 @@ public class Employee {
 
 	public static void main(String[] args){
 		
-		System.out.println("Welcome to Employee Wage builder");
+		
+		
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 
